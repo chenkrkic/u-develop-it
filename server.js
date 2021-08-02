a4bffc9f05db70f3911b1ecefcb7ea02c718dbd1
jiajie chen <ga-kit@hotmail.com>
2021-08-01 23:15:22 -0700

    join party data

--- server.js
+++ server.js
@@ -25,7 +25,13 @@ const db = mysql.createConnection(
 
 // GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id
+  WHERE candidates.id = ?`;
+
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
@@ -88,8 +94,12 @@ app.post('/api/candidate', ({ body }, res) => {
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
-  
+  const sql = `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id`;
+
     db.query(sql, (err, rows) => {
       if (err) {
         res.status(500).json({ error: err.message });
